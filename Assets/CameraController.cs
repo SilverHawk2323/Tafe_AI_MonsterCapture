8a92cd11a3b91f08047a82c0a9245dd69b20342f
SilverHawk2323 <anthony.moundalek@outlook.com>
2024-11-13 15:33:06 +1100

    refined Camera controller

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -17,17 +17,84 @@ public class CameraController : MonoBehaviour
 
 
     #region Oribit Variables
-    private Vector2 _orbitAngles = new Vector2(45f, 0);
+    private Vector2 _orbitAngles = new Vector2(-45f, 0);
     [Header("Orbit")]
     [SerializeField] private float _rotationSpeed = 90f;
     [SerializeField] private float _orbitDistance = 5f;
+    private float _lastDistance = -1f;
+    [SerializeField] private float _minVertAngle = -30f;
+    [SerializeField] private float _maxVertAngle = 60f;
+
+    private bool _orbitEnabled = true;
+
+    [SerializeField] private float _zoomSpeed = 0.5f;
     #endregion
+
+    #region Align
+    [Header("Align")]
+    [SerializeField, Min(0f)] private float alignDelay = 10f;
+    private float lastManualRotationTime = 0f;
+
+    #endregion
+
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
 
     private void Awake()
     {
         _focusPoint = _focus.position + _focusOffset;
         transform.localRotation = Quaternion.Euler(_orbitAngles);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_orbitEnabled)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+                _orbitEnabled = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                _orbitEnabled = true;
+            }
+        }
+
+        _orbitDistance -= Input.mouseScrollDelta.y * _zoomSpeed;
+
+        _orbitDistance = Mathf.Clamp(_orbitDistance, 3f, 10f);
+    }
+
+    /*private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(_focusPoint, transform.position,out hit ,_orbitDistance))
+        {
+            _lastDistance = _orbitDistance;
+            _orbitDistance = hit.distance;
+        }
+        else
+        {
+            if(_lastDistance >= 0f)
+            {
+                _orbitDistance = _lastDistance;
+                _lastDistance = -1f;
+            }
+        }
+    }*/
 
     //Camera controls should always be in late update
     private void LateUpdate()
@@ -38,22 +105,25 @@ public class CameraController : MonoBehaviour
 
         if (ManualRotation() || AutomaticRotation())
         {
+            ConstraintAngles();
             lookRotation = Quaternion.Euler(_orbitAngles);
         }
         Vector3 lookDirection = lookRotation * Vector3.forward;
 
         Vector3 lookPosition = _focusPoint - lookDirection * _orbitDistance;
-        transform.SetPositionAndRotation(lookPosition,lookRotation);
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
     }
 
     private bool ManualRotation()
     {
+        if (!_orbitEnabled) return false;
         Vector2 input = new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
 
         float e = 0.001f; //Deadzone
-        if(Mathf.Abs(input.x) > e || Mathf.Abs(input.y) > e)
+        if (Mathf.Abs(input.x) > e || Mathf.Abs(input.y) > e)
         {
             _orbitAngles += _rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
             return true;
         }
 
@@ -62,7 +132,20 @@ public class CameraController : MonoBehaviour
 
     private bool AutomaticRotation()
     {
-        return false;
+        if(Time.unscaledTime - lastManualRotationTime < alignDelay) return false;
+        Vector2 moveDelta = new Vector2(_focusPoint.x - _previousFocusPoint.x, _focusPoint.z - _previousFocusPoint.z);
+
+
+        if (moveDelta.sqrMagnitude < 0.0001f)
+        {
+            return false;
+        }
+
+        float headingAngle = GetAngle(moveDelta.normalized);
+
+        _orbitAngles.y = Mathf.MoveTowardsAngle(_orbitAngles.y, headingAngle, _rotationSpeed * Time.unscaledDeltaTime);
+
+        return true;
     }
 
     private void UpdateFocusPoint()
@@ -75,14 +158,14 @@ public class CameraController : MonoBehaviour
             float moveDistance = Vector3.Distance(targetFocusPoint, _previousFocusPoint);
             float t = 1f;
 
-            if(moveDistance > 0.01f && _focusCentering > 0f)
+            if (moveDistance > 0.01f && _focusCentering > 0f)
             {
                 t = Mathf.Pow(1f - _focusCentering, Time.unscaledDeltaTime);
             }
 
-            if(moveDistance > _focusRadius)
+            if (moveDistance > _focusRadius)
             {
-                t = Mathf.Min(t, _focusRadius/moveDistance);
+                t = Mathf.Min(t, _focusRadius / moveDistance);
             }
 
             _focusPoint = Vector3.Lerp(targetFocusPoint, _previousFocusPoint, t);
@@ -92,6 +175,42 @@ public class CameraController : MonoBehaviour
             _focusPoint = targetFocusPoint;
         }
 
-        
+
+    }
+
+    float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+
+        if (direction.x < 0f)
+        {
+            return 360f - angle;
+        }
+        else
+        {
+            return angle;
+        }
+    }
+
+    void ConstraintAngles()
+    {
+        _orbitAngles.x = Mathf.Clamp(_orbitAngles.x, _minVertAngle, _maxVertAngle);
+
+        if(_orbitAngles.y < 0f)
+        {
+            _orbitAngles.y += 360f;
+        }
+        else if(_orbitAngles.y < 360f)
+        {
+            _orbitAngles.y -= 360f;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if(_maxVertAngle < _minVertAngle)
+        {
+            _maxVertAngle = _minVertAngle;
+        }
     }
 }
